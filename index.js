f220644d62c7395be13f73dd48439622646ecb00
nyaga-gitonga <uptronafrica@gmail.com>
2022-09-13 21:19:26 +0300

    Command Line Interface

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 var server = require('./lib/server');
 var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 
 // Declare the app
 var app = {};
@@ -18,6 +19,11 @@ app.init = function(){
 
   // Start the workers
   workers.init();
+
+  //Start the cli but make sure it starts last
+  setTimeout(function(){
+  cli.init();
+  },50);
 
 };
 
