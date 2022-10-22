8e70210dce66903f235a7da24948f739d1ff755e
nyaga-gitonga <uptronafrica@gmail.com>
2022-10-23 02:18:59 +0300

    REPL cli

---
+++ misc/repl.js
@@ -0,0 +1,22 @@
+/*
+* Example REPL server
+*
+* Take in the word "fizz" and log out "Buzz"
+*/
+
+//Dependencies
+var repl = require('repl');
+
+//Start the REPL
+repl.start({
+    'prompt': '>',
+    'eval': function(str){
+        //Evaluation function for incoming inputs
+        console.log("At the evaluation stage",str);
+        //If the user said fizz say buzz back to them
+
+        if(str.indexOf('fizz')> -1){
+            console.log('buzz');
+        }
+    }
+})
